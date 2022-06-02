e5273c414561e0c4ffddbd2e889a8b7686627aad
kramrekwob <m.bowker422@gmail.com>
2022-06-02 09:41:58 -0400

    pre-wedding coding

---
+++ 8 Kyu Codewars/Calculate average.js
@@ -0,0 +1,8 @@
+function findAverage(array) {
+    let total=0;
+    if (array.length==0) return 0;
+    for (let i=0; i < array.length; i++){
+      total += array[i];
+    }
+    return total/array.length;
+  }
---
+++ 8 Kyu Codewars/Sum Array.js
@@ -0,0 +1,6 @@
+// Sum Numbers
+function sum (numbers) {
+    "use strict";
+    return numbers.reduce((acc, num) => {return acc+num},0 );
+    
+};
