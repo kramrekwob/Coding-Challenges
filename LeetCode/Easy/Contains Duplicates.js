b49e2c8ee9081f8789780b727331881776c0f8a7
kramrekwob <m.bowker422@gmail.com>
2022-10-19 21:08:46 -0400

    Update Contains Duplicates.js

--- LeetCode/Easy/Contains Duplicates.js
+++ LeetCode/Easy/Contains Duplicates.js
@@ -2,6 +2,7 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+// First Pass, Speed is 0(N Squared), very bad.  Very little memory usage though
  var containsDuplicate = function(nums) {
     for (let i=0; i<nums.length; i++){
         for (let j=i+1; j<nums.length; j++)
@@ -10,4 +11,19 @@
     return false;
 };
 //Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
-//small change
+
+//using a cache O(N) Takes Space Too
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+ var containsDuplicate = function(nums) {
+    let cache = {}
+    for (let i=0; i<nums.length; i++){
+        if (!cache[nums[i]]) {
+            cache[nums[i]] = true;
+        }
+        else return true;
+    }
+    return false;
+}
